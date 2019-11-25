c19186bc81ce22c267f6db622f3710f783545c50
starovp <philipp@starovp.com>
2019-11-25 14:31:57 -0500

    Problem 1. Two sum

---
+++ problem-1-two-sum/twosum.py
@@ -0,0 +1,39 @@
+"""
+1. Two Sum
+
+Given an array of integers, return indices 
+of the two numbers such that they add up 
+to a specific target.
+
+You may assume that each input would have 
+exactly one solution, and you may not use 
+the same element twice.
+"""
+
+class Solution:
+    def twoSum(self, nums, target):
+        # nums : List[int]
+        # target: int
+        # return: List[int]
+
+        # Create a map
+        num_map = {}
+
+        # Iterate over list
+        for i in range(len(nums)):
+            # The target value minus current list value is our map target
+            map_target = target - nums[i]
+
+            # If a record exists for the matching map target, then return the
+            # current location and the map target location 
+            if num_map.get(map_target) is not None:
+                return [num_map.get(map_target), i]
+            # Else create a dict entry of our current list value and the current spot
+            else:
+                num_map[nums[i]] = i
+            
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.twoSum([2,7,11,15],9))
+    print(solution.twoSum([1,2,3,4,5,6,7],13))
+    print(solution.twoSum([2,4,6,7],10))
